48981320deafb159e6967a57086e2aba9a86de1b
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-17 19:18:27 +0300

    modify main.py file

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
-squares = {i: i * i for i in range(10) if i != 5}
-print(squares)
+# squares = {i: i * i for i in range(10) if i != 5}
+# print(squares)
+
+# squares = {i: i * i for i in range(10) if i % 2}
+# print(squares)
+
+# values = ["a", "b", "c"]
+# for count, value in enumerate(values):
+#     print(count, value)
+
+# my_tuple = (1, 2, 3)
+# num1, num2, num3 = my_tuple
+# print(my_tuple)
+
+# values = ["a", "b", "c"]
+# for value in enumerate(values):
+#     # print(value)
+#     count, value = value
+
+    # print (f"aflkj {count} fdsa {value}")
+
+# def even_items(numbers: list):
+#     return {i + 1 : v for i, v in enumerate(numbers, start=1) if not i % 2}
+
+# seq = list(range(1, 11))
+
+# print(even_items(seq))
+
+# def even_items(numbers: list):
+#     new_list = []
+#     for i, v in enumerate(numbers, start=1):
+#         if not i % 2:
+#             new_list.append(v)
+#     return(new_list)
+
+# seq = list(range(1, 145))
+# print(even_items(seq))
+
+# 3
+# text = 'In this lecture we will review some additional functionalities of python built-in data structures.'
+# words_with_e = [word for word in text.split(" ") if "e" in word]
+# print(len(words_with_e))
+
+# 4
+# text = 'In this lecture we will review some additional functionalities of python built-in data structures.'
+# words_with_e = [word for word in text.split(" ") if len(word) > 5]
+# print(len(words_with_e))
+
+
