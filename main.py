a8b865a8a8ffd4ef70e9e509044b9f073553f877
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-18 19:00:27 +0300

    modify main.py file

--- main.py
+++ main.py
@@ -31,7 +31,7 @@
 #     for i, v in enumerate(numbers, start=1):
 #         if not i % 2:
 #             new_list.append(v)
-#     return(new_list)
+#     return new_list
 
 # seq = list(range(1, 145))
 # print(even_items(seq))
@@ -41,16 +41,16 @@
 # words_with_e = [word for word in text.split(" ") if "e" in word]
 # print(len(words_with_e))
 
-# 4
+
 # text = 'In this lecture we will review some additional functionalities of python built-in data structures.'
-# words_with_e = [word for word in text.split(" ") if len(word) > 5]
+# words_with_e = [word for word in text.split() if len(word) > 5]
 # print(len(words_with_e))
 
 # 5
 # text = 'In this lecture we will review some additional functionalities of python built-in data structures.'
 # letter_dict = {}
 # for letter in text:
-#     if letter != " " and letter != "-" and letter != ".":
+#     if letter.isalpha():
 #         letter_dict[letter] = letter_dict.get(letter, 0) + 1
 # print(letter_dict)
 
@@ -65,4 +65,15 @@
 #     else:
 #         print("Not perfect square")
 
-# is_perfect_square(0)
+# is_perfect_square(4)
+
+# def common(a, b, c):
+#     for item_a in a:
+#         for item_b in b:
+#             for item_c in c:
+
+
+        
+    
+
+# print(common([1,2,2,3],[5,3,2,2],[7,3,2,2]))
